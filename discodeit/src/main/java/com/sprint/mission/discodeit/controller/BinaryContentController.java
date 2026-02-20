2f44ada522ddacb2a7557b99ea47cb41608629dd
apple <kjw010900@gmail.com>
2026-02-20 14:18:36 +0900

    refactor(BinaryContentController):  - update Controller to RestController annotations  - delete unused import

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -6,19 +6,16 @@ import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.List;
 import java.util.UUID;
+import org.springframework.web.bind.annotation.RestController;
 
-@ResponseBody
+
 @RequestMapping("/api/binaryContent")
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class BinaryContentController {
 
   private final BinaryContentService binaryContentService;
