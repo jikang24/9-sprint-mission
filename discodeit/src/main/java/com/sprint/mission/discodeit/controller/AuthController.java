074f70ac19cdfeb68b0007c26b77fc6f9d897c36
apple <kjw010900@gmail.com>
2026-03-04 23:18:52 +0900

    delete: remove unused imports

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -8,12 +8,10 @@ import com.sprint.mission.discodeit.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 
