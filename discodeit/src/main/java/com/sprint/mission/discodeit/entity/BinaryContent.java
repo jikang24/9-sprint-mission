5babe574493fbc2511924aac6e5dd13857e5d9cb
apple <kjw010900@gmail.com>
2026-03-09 11:37:17 +0900

    feat: add Column name for file name and content type in BinaryContent entity

--- discodeit/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -14,11 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BinaryContent extends BaseEntity {
 
-  @Column(nullable = false)
+  @Column(name = "file_name", nullable = false)
   private String fileName;
   @Column(nullable = false)
   private Long size;
-  @Column(length = 100, nullable = false)
+  @Column(name = "content_type", length = 100, nullable = false)
   private String contentType;
 
   public BinaryContent(String fileName, Long size, String contentType) {
