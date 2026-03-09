c41d788f0b8fb379b17c07379b7535f908da0f6b
apple <kjw010900@gmail.com>
2026-03-09 13:13:01 +0900

    feat: refactor binary content API - update schema response to dto - update download method content parameter

--- discodeit/src/main/java/com/sprint/mission/discodeit/controller/api/BinaryContentApi.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/controller/api/BinaryContentApi.java
@@ -1,6 +1,6 @@
 package com.sprint.mission.discodeit.controller.api;
 
-import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Tag(name = "Binary Content", description = "Binary Content API")
 public interface BinaryContentApi {
@@ -23,7 +23,7 @@ public interface BinaryContentApi {
   @ApiResponses(value = {
       @ApiResponse(
           responseCode = "200", description = "BinaryContent 단건 조회 완료",
-          content = @Content(schema = @Schema(implementation = BinaryContent.class))
+          content = @Content(schema = @Schema(implementation = BinaryContentDto.class))
       ),
       @ApiResponse(
           responseCode = "404", description = "BinaryContent를 찾을 수 없음",
@@ -38,10 +38,26 @@ public interface BinaryContentApi {
   @ApiResponses(value = {
       @ApiResponse(
           responseCode = "200", description = "BinaryContent 전체 조회",
-          content = @Content(array = @ArraySchema(schema = @Schema(implementation = BinaryContent.class)))
+          content = @Content(array = @ArraySchema(schema = @Schema(implementation = BinaryContentDto.class)))
       )
   })
   ResponseEntity<List<BinaryContent>> findAllByIdIn(
       @Parameter(description = "조회할 첨부 파일 ID 목록") List<UUID> binaryContentIds
+  );
+
+
+  @Operation(summary = "파일 download")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200", description = "파일 download 성공",
+          content = @Content
+      ),
+      @ApiResponse(
+          responseCode = "404", description = "download할 파일이 존재하지 않음",
+          content = @Content(examples = @ExampleObject(value = "BinaryContent with id {binaryContentId} not found"))
+      )
+  })
+  ResponseEntity<?> download(
+      @PathVariable("binaryContentId") UUID binaryContentId
   );
 }
