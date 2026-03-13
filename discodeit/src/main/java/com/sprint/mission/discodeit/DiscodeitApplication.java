57c0be8bfc27242c680eae4ff328a186da0ed852
apple <kjw010900@gmail.com>
2026-03-14 00:07:37 +0900

    test: test Application after clean merge conflicts

--- discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -4,7 +4,6 @@ package com.sprint.mission.discodeit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DiscodeitApplication {
 
