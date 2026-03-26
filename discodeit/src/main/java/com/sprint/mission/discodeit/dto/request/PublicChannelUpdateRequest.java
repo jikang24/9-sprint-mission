57c677eab8ea2f986bb27e1ff418688aaeca0d10
apple <kjw010900@gmail.com>
2026-03-26 10:50:31 +0900

    feat: update validation annotations and add size

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
@@ -1,7 +1,12 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.Size;
+
 public record PublicChannelUpdateRequest(
+    @Size(min = 1, max = 20, message = "채널 이름은 1자 이상 20자 이하여야 합니다.")
     String newName,
+
+    @Size(max = 30, message = "채널 설명은 30자 이하로만 가능합니다")
     String newDescription
 ) {
 
