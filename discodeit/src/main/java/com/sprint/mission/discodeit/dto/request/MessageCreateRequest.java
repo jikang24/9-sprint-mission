93658df716458120fad1a1aadda52b36006ec950
apple <kjw010900@gmail.com>
2026-03-26 10:50:31 +0900

    feat: update validation annotations and add size

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/MessageCreateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/MessageCreateRequest.java
@@ -1,10 +1,19 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record MessageCreateRequest(
+    @NotBlank(message = "공백의 메시지는 보낼 수 없습니다.")
+    @Size(max = 1000, message = "메시지는 1000자 이하로 보낼 수 있습니다")
     String content,
+    
+    @NotNull(message = "채널 ID는 필수입니다")
     UUID channelId,
+
+    @NotNull(message = "작성자 ID는 필수입니다")
     UUID authorId
 ) {
 
