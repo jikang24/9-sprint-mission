370a99d1d5108e977976397b04e0032ce914846c
apple <kjw010900@gmail.com>
2026-04-07 13:33:08 +0900

    refactor: update error codes and add http status to exception details

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/response/ErrorResponse.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/response/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.dto.response;
 
+import com.sprint.mission.discodeit.exception.detail.ExceptionDetail;
 import java.time.Instant;
 import java.util.Map;
 
@@ -7,7 +8,7 @@ public record ErrorResponse(
     Instant timestamp,      // 예외 발생 시각
     String code,            // 에러 코드명 (예: USER_NOT_FOUND)
     String message,         // 에러 메시지
-    Map<String, Object> details,  // 추가 정보
+    ExceptionDetail details,  // 추가 정보
     String exceptionType,   // 발생한 예외의 클래스 이름
     int status              // HTTP 상태코드
 ) {
