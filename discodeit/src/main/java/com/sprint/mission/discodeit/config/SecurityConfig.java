b533866b20dd7efa8b357e6bd98bb9fd270716e7
apple <kjw010900@gmail.com>
2026-05-13 16:28:59 +0900

    feat: update security config

--- discodeit/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.handler.LoginFailureHandler;
+import com.sprint.mission.discodeit.handler.LoginSuccessHandler;
 import com.sprint.mission.discodeit.handler.SpaCsrfTokenRequestHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,7 +17,11 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final LoginSuccessHandler loginSuccessHandler;
+  private final LoginFailureHandler loginFailureHandler;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -24,8 +31,20 @@ public class SecurityConfig {
             .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
         )
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
-            .anyRequest().authenticated()
+                .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+//            .requestMatchers(
+//                "/swagger-ui/**",
+//                "/v3/api-docs/**"
+//            ).permitAll()
+                .anyRequest()
+//                .authenticated()
+                .permitAll()
+        )
+        .formLogin(login -> login
+            .loginProcessingUrl("/api/auth/login")
+            .successHandler(loginSuccessHandler)
+            .failureHandler(loginFailureHandler)
+            .permitAll()
         );
     return http.build();
   }
