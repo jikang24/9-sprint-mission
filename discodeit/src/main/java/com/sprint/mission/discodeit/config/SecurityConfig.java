bfd39d7ae80d4a39ebc183d3dcdb8e2443e5cf0d
apple <kjw010900@gmail.com>
2026-05-13 10:36:57 +0900

    create: add csrf token handling and csrf token repository configuration

--- discodeit/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.sprint.mission.discodeit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.handler.SpaCsrfTokenRequestHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @Configuration
@@ -13,6 +20,13 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
+        );
     return http.build();
   }
+
+
 }
