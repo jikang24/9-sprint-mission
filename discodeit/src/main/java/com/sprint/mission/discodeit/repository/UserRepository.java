40add6d18a5feb6f8ee19b7484edfb710eb2b33b
apple <kjw010900@gmail.com>
2026-05-13 16:26:32 +0900

    feat: add email-based user lookup and update password validation

--- discodeit/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -11,6 +11,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
   Optional<User> findByUsername(String username);
 
+  Optional<User> findByEmail(String email);
+
   boolean existsByEmail(String email);
 
   boolean existsByUsername(String username);
