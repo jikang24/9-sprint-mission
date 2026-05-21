d894ccb845401ea235fe30ce7eba7aafaed559fa
apple <kjw010900@gmail.com>
2026-05-21 15:10:31 +0900

    feat: update UserServiceTest to use UserRepository instead of UserStatusRepository

--- discodeit/src/test/java/com/sprint/mission/discodeit/service/UserServiceTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/service/UserServiceTest.java
@@ -16,7 +16,6 @@ import com.sprint.mission.discodeit.fixture.UserFixture;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.BinaryContentRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
-import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.basic.BasicUserService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.util.Optional;
